51450ff7ec3ef83a70b08662dbe214ffd5e3209f
Episkey <paul.beziat66@gmail.com>
2019-05-20 17:52:54 +0200

    projetcreate api

---
+++ src/main/java/com/wildcodeschool/ApiCreate/ApiCreateApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.ApiCreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCreateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCreateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/ApiCreate/ApiCreateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wildcodeschool.ApiCreate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiCreateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
